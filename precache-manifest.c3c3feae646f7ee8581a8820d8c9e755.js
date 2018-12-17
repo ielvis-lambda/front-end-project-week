0cfc993e99b55ef2ea5fb063e08d85264a62cfb2
Elvis Ibarra <ielvisd@gmail.com>
2018-12-17 15:08:29 -0800

    Updates

--- precache-manifest.c3c3feae646f7ee8581a8820d8c9e755.js
+++ precache-manifest.c3c3feae646f7ee8581a8820d8c9e755.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/front-end-project-week/static/js/runtime~main.9c13ef43.js"
   },
   {
-    "revision": "9e556ce03dffdcc54831",
-    "url": "/front-end-project-week/static/js/main.9e556ce0.chunk.js"
+    "revision": "dbd7a626821f32875548",
+    "url": "/front-end-project-week/static/js/main.dbd7a626.chunk.js"
   },
   {
     "revision": "edacebbae3030ac6d697",
     "url": "/front-end-project-week/static/js/1.edacebba.chunk.js"
   },
   {
-    "revision": "9e556ce03dffdcc54831",
+    "revision": "dbd7a626821f32875548",
     "url": "/front-end-project-week/static/css/main.482c231a.chunk.css"
   },
   {
-    "revision": "b3a59793e31663ab2f3145ac1b1af2b5",
+    "revision": "a10c9bf1ef0061c8434d482a3d10e47d",
     "url": "/front-end-project-week/index.html"
   }
 ];
