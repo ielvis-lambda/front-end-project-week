3ccfd7e409e98f8720d3ea9f7fd24327510989d4
Elvis Ibarra <ielvisd@gmail.com>
2018-12-21 18:02:55 -0800

    Updates

--- precache-manifest.a4b231041b3c4eab2986e4480aebf948.js
+++ precache-manifest.a4b231041b3c4eab2986e4480aebf948.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/front-end-project-week/static/js/runtime~main.9c13ef43.js"
   },
   {
-    "revision": "a40b27e0807cf5312df9",
-    "url": "/front-end-project-week/static/js/main.a40b27e0.chunk.js"
+    "revision": "e6b6431130727b6adcb5",
+    "url": "/front-end-project-week/static/js/main.e6b64311.chunk.js"
   },
   {
     "revision": "929dd6093cc838b9b10d",
     "url": "/front-end-project-week/static/js/1.929dd609.chunk.js"
   },
   {
-    "revision": "a40b27e0807cf5312df9",
+    "revision": "e6b6431130727b6adcb5",
     "url": "/front-end-project-week/static/css/main.0689f3b5.chunk.css"
   },
   {
-    "revision": "7cd9f8624590bf05fc7fe76e81f45d30",
+    "revision": "334f53aad4fcc826431ba816569f876e",
     "url": "/front-end-project-week/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/front-end-project-week/precache-manifest.d8110ef4f328f8b951401cb4223fc8f2.js"
+  "/front-end-project-week/precache-manifest.a4b231041b3c4eab2986e4480aebf948.js"
 );
 
 workbox.clientsClaim();
